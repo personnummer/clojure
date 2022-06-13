9fffb0ba503f0a7a2be62b0b6fc9c1c061f7fed6
Simon Sawert <simon@sawert.se>
2022-06-13 23:18:37 +0200

    Add support to parse string

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 user=> (use 'personnummer.personnummer :reload)
 user=> (load-file "test/personnummer/personnummer_test.clj")
 ```
+
+Or with [`lein`](https://leiningen.org/)
+
+```sh
+% lein test
+```
--- src/personnummer/core.clj
+++ src/personnummer/core.clj
@@ -1,8 +1,30 @@
 (ns personnummer.core
-  (:require [clojure.string :refer (join split)]))
+  (:require [clojure.string :refer (join split)]
+            [clj-time.core :as t]))
+
+(def pnr-re
+  #"^([0-9]{2})?([0-9]{2})([0-9]{2})([0-9]{2})([-+])?([0-9]{3})([0-9])?$")
 
 (defrecord Personnummer
            [date serial control separator coordination])
+
+(defn personnummer
+  "Create a new instance of Personnummer."
+  [input]
+  (let [date (re-matches pnr-re input)
+        century (read-string (nth date 1 "20"))
+        year (read-string (nth date 2 "0"))
+        month (read-string (nth date 3 "0"))
+        day (read-string (nth date 4 "0"))
+        divider (nth date 5 "-")
+        serial (read-string (nth date 6 "000"))
+        control (read-string (nth date 7 "0"))]
+    (->Personnummer
+     (t/date-time (+ (* century 100) year) month day 0 0 0 0)
+     serial
+     control
+     divider
+     (> day 60))))
 
 (defn luhn-sum [digits]
   (apply
@@ -18,3 +40,12 @@
   (- 10
      (mod
       (luhn-sum digits) 10)))
+
+(defn luhn-string [string]
+  (luhn (map read-string (split string #""))))
+
+(defn valid [pnr]
+  (and
+   (not= nil? (:date pnr))
+   (> (:serial pnr) 0)
+   (= (luhn-string "900101001") (:control pnr))))
