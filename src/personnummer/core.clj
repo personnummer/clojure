d424947ff46a72e77c5d15cddf2fecf9877e0c5f
Simon Sawert <simon@sawert.se>
2022-06-14 20:19:52 +0200

    Fully working validation

--- src/personnummer/core.clj
+++ src/personnummer/core.clj
@@ -1,30 +1,26 @@
 (ns personnummer.core
   (:require [clojure.string :refer (join split)]
-            [clj-time.core :as t]))
-
-(def pnr-re
-  #"^([0-9]{2})?([0-9]{2})([0-9]{2})([0-9]{2})([-+])?([0-9]{3})([0-9])?$")
+            [clj-time.core :as t]
+            [clj-time.format :as tf]))
 
 (defrecord Personnummer
            [date serial control separator coordination])
 
+(def pnr-re
+  #"^([0-9]{2})?([0-9]{2})([0-9]{2})([0-9]{2})([-+])?([0-9]{3})([0-9])?$")
+
 (defn personnummer
   "Create a new instance of Personnummer."
   [input]
-  (let [date (re-matches pnr-re input)
-        century (read-string (nth date 1 "20"))
-        year (read-string (nth date 2 "0"))
-        month (read-string (nth date 3 "0"))
-        day (read-string (nth date 4 "0"))
-        divider (nth date 5 "-")
-        serial (read-string (nth date 6 "000"))
-        control (read-string (nth date 7 "0"))]
+  (if-some [[_ century year month day divider serial control]
+            (re-matches pnr-re input)]
     (->Personnummer
-     (t/date-time (+ (* century 100) year) month day 0 0 0 0)
-     serial
-     control
+     (t/date-time (+ (* (read-string century) 100) (read-string year)) (read-string month) (read-string day) 0 0 0 0)
+     (read-string serial)
+     (read-string control)
      divider
-     (> day 60))))
+     (> (read-string day) 60))
+    (Personnummer. nil 0 0 "" false)))
 
 (defn luhn-sum [digits]
   (apply
@@ -35,6 +31,11 @@
                (map * digits
                     (cycle [2 1])))
          #""))))
+
+(defn format-for-luhn [pnr]
+  (format "%s%03d"
+          (tf/unparse (tf/formatter "yyMMdd") (:date pnr))
+          (:serial pnr)))
 
 (defn luhn [digits]
   (- 10
@@ -48,4 +49,4 @@
   (and
    (not= nil? (:date pnr))
    (> (:serial pnr) 0)
-   (= (luhn-string "900101001") (:control pnr))))
+   (= (luhn-string (format-for-luhn pnr)) (:control pnr))))
